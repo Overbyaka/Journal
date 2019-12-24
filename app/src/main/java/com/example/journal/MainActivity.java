ecb5c815ce3e2bf389e2fdab255fcd86cc216f3a
Eugene <malenkayaelfiyka@yandex.ru>
2019-12-24 23:05:22 +0300

    0.1 Copy diary
    Скопирован дневник, на основе которого будет строится классный журнал

--- app/src/main/java/com/example/journal/MainActivity.java
+++ app/src/main/java/com/example/journal/MainActivity.java
@@ -2,7 +2,14 @@ package com.example.journal;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,"create",Toast.LENGTH_SHORT).show();
+    }
+
+    String LOGIN= "Mur";
+    String PASSWORD = "Meow";
+
+    public void entering(View view){
+        String login = ((EditText)findViewById(R.id.login)).getText().toString();
+        String password = ((EditText)findViewById(R.id.password)).getText().toString();
+        TextView result = (TextView) findViewById(R.id.result);
+        if(LOGIN.equals(login) && PASSWORD.equals(password)){
+            result.setTextColor(Color.GREEN);
+            result.setText("Верно");
+            Intent i = new Intent(MainActivity.this,MainActivity2.class);
+            startActivity(i);
+        }
+        else {
+            result.setTextColor(Color.RED);
+            result.setText("Вы ошиблись в логине или пароле");
+        }
     }
 }
---
+++ app/src/main/java/com/example/journal/MainActivity2.java
@@ -0,0 +1,37 @@
+package com.example.journal;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.CalendarView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+import java.util.Calendar;
+
+public class MainActivity2 extends Activity {
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanseState){
+        super.onCreate(savedInstanseState);
+        setContentView(R.layout.activity_main2);
+
+        final CalendarView calendarView = (CalendarView) findViewById(R.id.calendar);
+        calendarView.setFirstDayOfWeek(Calendar.MONDAY);
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+
+            @Override
+            public void onSelectedDayChange(CalendarView view, int year,
+                                            int month, int dayOfMonth) {
+                int mYear = year;
+                int mMonth = month;
+                int mDay = dayOfMonth;
+                Intent i = new Intent(MainActivity2.this,OpenDay.class);
+                i.putExtra("day","" + mDay);
+                i.putExtra("month","" + mMonth);
+                i.putExtra("year","" + mYear);
+                startActivity(i);
+            }
+        });
+    }
+}
