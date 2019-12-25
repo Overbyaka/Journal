462c1f205e57e18b3757c5f0032909073ac75cd8
Eugene <malenkayaelfiyka@yandex.ru>
2019-12-26 02:31:43 +0300

    1.0 Without base
    Не дождавшись базы, основала программу на работу с файлами. Поэтому многих функций до сих пор нет.

--- app/src/main/java/com/example/journal/Journal.java
+++ app/src/main/java/com/example/journal/Journal.java
@@ -1,34 +1,85 @@
 package com.example.journal;
 
 import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 import android.widget.TableLayout;
 import android.widget.TableRow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
 public class Journal extends Activity {
+
+    SharedPreferences sPref;
+    int currentStudent = 1;
+    final int n = 16;
+    final int m = 3;
+    int [][]array = new int[n][m];
+    final String []namefile= {"student1","student2","student3"};
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.journal);
+        final TextView tvs = (TextView) findViewById(R.id.student);
+        Button prev = (Button) findViewById(R.id.prev);
+        Button next = (Button) findViewById(R.id.next);
+        Button save = (Button) findViewById(R.id.save);
+        final String access = getIntent().getStringExtra("access");
+        if(access.equals("student")) {
+            prev.setVisibility(View.GONE);
+            next.setVisibility(View.GONE);
+            save.setVisibility(View.GONE);
+        }else{
+            prev.setVisibility(View.VISIBLE);
+            next.setVisibility(View.VISIBLE);
+            save.setVisibility(View.VISIBLE);
+        }
 
-        final int n = 16;
-        final int m = 6;
-        final int [][]array = new int[n][m];
+        String oldText = loadText(namefile[currentStudent-1]);
+        tvs.setText("Студент " + currentStudent);
 
+        if(oldText.equals("")){
         for(int i = 0; i < n;i++){
             for(int j = 0; j < m;j++){
                 array[i][j]=0;
             }
+        }}
+        else{
+            for(int i = 0; i < n;i++){
+                String[]sentence = oldText.split(" ");
+                for(int j = 0; j < m;j++){
+                    array[i][j]=Integer.parseInt(sentence[i*m+j]);
+                }
+            }
         }
 
+        prev.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setCurrentStudentPrev(tvs);
+            }
+        });
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                setCurrentStudentNext(tvs);
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveText(namefile[currentStudent-1],array);
+            }
+        });
+
         TableLayout tableLayout = (TableLayout) findViewById(R.id.table);
-
-
         for(int i = 1; i <= n;i++){
 
             TableRow row = new TableRow(this);
@@ -89,13 +140,14 @@ public class Journal extends Activity {
             row.addView(tv,0);
             for(int j = 1; j <=m;j++){
                 final Button bt =new Button(this);
-                bt.setWidth(50);
+                bt.setHeight(50);
                 final int finalI = i;
                 final int finalJ = j;
                 bt.setText("" + array[finalI-1][finalJ-1]);
                 bt.setOnClickListener(new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+                        if(!access.equals("student")){
                         int count = Integer.parseInt(bt.getText().toString());
                         if(count==5)
                             count = 0;
@@ -103,11 +155,48 @@ public class Journal extends Activity {
                             count++;
                         bt.setText("" + count);
                         array[finalI-1][finalJ-1] = count;
+                        }
                     }
                 });
                 row.addView(bt,j);
             }
-            tableLayout.addView(row,1);
+            tableLayout.addView(row,i);
         }
+    }
+    private void saveText(String namefile,int[][]array){
+        sPref = getSharedPreferences(namefile,MODE_PRIVATE);
+        SharedPreferences.Editor ed = sPref.edit();
+        String savetext = "";
+        for(int i = 0; i < n;i++){
+            for(int j = 0; j < m;j++){
+                savetext+=(array[i][j] + " ");
+            }
+        }
+        ed.putString(namefile,savetext);
+        ed.commit();
+        Toast.makeText(Journal.this,"Text saved",Toast.LENGTH_LONG).show();
+    }
+    private String loadText(String namefile){
+        sPref = getSharedPreferences(namefile,MODE_PRIVATE);
+        return sPref.getString(namefile,"");
+    }
+
+    void setCurrentStudentPrev(TextView tv){
+        if(currentStudent == 1){
+            currentStudent = 3;
+        } else{
+            currentStudent--;
+        }
+        tv.setText("Студент " + currentStudent);
+        onCreate(new Bundle());
+    }
+    void setCurrentStudentNext(TextView tv){
+        if(currentStudent == 3){
+            currentStudent = 1;
+        } else{
+            currentStudent++;
+        }
+        tv.setText("Студент " + currentStudent);
+        onCreate(new Bundle());
     }
 }
